cea0ef805b1ea252ad77e68c760ad4f4ba3fbf79
iMAK73 <muginovak73@gmail.com>
2018-03-25 16:55:14 +0300

    branch-2-2 commit2

--- prj_git_learn01/prj_git_learn01/IMPL_sample2.cs
+++ prj_git_learn01/prj_git_learn01/IMPL_sample2.cs
@@ -8,6 +8,11 @@ namespace prj_git_learn01
 {
     class IMPL_sample2 : INTF_sample2
     {
+        public void Method2_is2()
+        {
+            Console.WriteLine("impl 2-2");
+        }
+
         public void Method_is2(string param)
         {
             // some inmplementation
